b71c3aeecc1a966dfd2851b350b32590fd2b0826
mahmudunnabikajal <mahmudunnabikajal@gmail.com>
2025-07-04 20:36:31 +0600

    refactor: remove unused

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::middleware(['auth'])->group(function () {
 
     // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Todos Resource
     Route::resource('todos', TodoController::class);
